d6f90c6a51dc557e9d272347f500d6dfae6a57ac
phatiacy <phatiacyy@gmail.com>
2024-06-18 14:28:45 +0300

    added image gallery

--- src/lib/components/custom/auction-platform-project.svelte
+++ src/lib/components/custom/auction-platform-project.svelte
@@ -1,32 +1,53 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import white_logo from '$lib/assets/white_logo.png';
-    import boat from '$lib/assets/boat.jpg';
-    import Icon from '@iconify/svelte';
+	import boat from '$lib/assets/boat.jpg';
+	import Icon from '@iconify/svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import AuctionPlatformGallery from './image-galleries/auction-platform-gallery.svelte';
 </script>
 
 <main>
-    <div class="flex flex-col">
-			<div class="grid grid-cols-3 gap-4">
-				<div class="self-center border-2 rounded-xl justify-self-center shadow-lg">
-					<img alt="logo" src={boat} class="logo hidden md:block lg:block grayscale blur-[2px] object-contain p-0.5 rounded-xl hover:grayscale-0 hover:blur-0 duration-300" style="width: 350px;" />
-				</div>
-				<div class="flex flex-col gap-2 col-span-3 md:col-span-2 lg:col-span-2">
-					<h3 class="mb-4 text-2xl text-white">SvelteKit Auction Platform</h3>
-					<p class="text-white">
-						This is a project featuring a custom Python API with FastAPI, SvelteKit and AWS integration. The winner of each auction is determined by a custom AI model that was tweaked with Weka.
-					</p>
-					<p class="text-white">
-						After the winner of an auction is chosen, the user has to verify their identity against a valid document using AWS Rekognition. Furthermore, there is digital signature verification for extra security. The code is available on GitHub.
-					</p>
-					<div class="mt-4 flex flex-row gap-2">
-						<Button class="hover:underline hover:bg-primary underline-offset-4 " href=https://github.com/sergiulache/licentav2>
-							<Icon icon="mdi-github" class="mr-2" width="2rem" color="#8a8a8a" />
-							View Code
-						</Button>
-						<!-- <Button class="bg-accent hover:bg-muted-foreground">Live Demo</Button> -->
-					</div>
+	<div class="flex flex-col">
+		<div class="lg:grid lg:grid-cols-3 gap-4 flex flex-col">
+			<div class="self-start lg:self-center justify-self-center rounded-xl border-2 shadow-lg flex flex-row">
+				<Dialog.Root>
+					<Dialog.Trigger class="p-0.5">
+						<img
+							alt="logo"
+							src={boat}
+							class="logo rounded-xl object-contain  blur-[2px] grayscale duration-300 hover:blur-0 hover:grayscale-0 md:block lg:block"
+							style="width: 350px;"
+						/>
+					</Dialog.Trigger>
+					<Dialog.Content>
+						<AuctionPlatformGallery/>
+					</Dialog.Content>
+				</Dialog.Root>
+			</div>
+			<div class="col-span-3 flex flex-col gap-2 md:col-span-2 lg:col-span-2">
+				<h3 class="mb-4 text-2xl text-white">SvelteKit Auction Platform</h3>
+				<p class="text-white">
+					This is a project featuring a custom Python API with FastAPI, SvelteKit and AWS
+					integration. The winner of each auction is determined by a custom AI model that was
+					tweaked with Weka.
+				</p>
+				<p class="text-white">
+					After the winner of an auction is chosen, the user has to verify their identity against a
+					valid document using AWS Rekognition. Furthermore, there is digital signature verification
+					for extra security. The code is available on GitHub.
+				</p>
+				<div class="mt-4 flex flex-row gap-2">
+					<Button
+						class="underline-offset-4 hover:bg-primary hover:underline "
+						href="https://github.com/sergiulache/licentav2"
+					>
+						<Icon icon="mdi-github" class="mr-2" width="2rem" color="#8a8a8a" />
+						View Code
+					</Button>
+					<!-- <Button class="bg-accent hover:bg-muted-foreground">Live Demo</Button> -->
 				</div>
 			</div>
 		</div>
+	</div>
 </main>
---
+++ src/lib/components/custom/image-galleries/auction-platform-gallery.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import white_logo from '$lib/assets/white_logo.png';
+	import boat from '$lib/assets/boat.jpg';
+	import Icon from '@iconify/svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
+
+	// repeat the boat image 7 times
+	const images = Array.from({ length: 7 }, (_, i) => boat);
+</script>
+
+<main>
+	<div class="grid grid-cols-3">
+		{#each images as image}
+			<div class="m-3 flex flex-row rounded-xl border-2 shadow-lg">
+				<img
+					alt="logo"
+					src={image}
+					class="logo hidden rounded-xl object-contain duration-300 md:block lg:block"
+					style="width: 350px;"
+				/>
+			</div>
+		{/each}
+	</div>
+</main>
